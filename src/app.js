2822b2e7bdd9a2a73da97fed22d482a72b73c1ba
Aleksandr Oliferchuk <oliferchuk.aleksandr@gmail.com>
2024-10-30 15:07:30 +0200

    App logic
    did icreasing aechievements and implement storage

--- src/app.js
+++ src/app.js
@@ -1,6 +1,28 @@
 const circle = document.querySelector('#circle');
 const score = document.querySelector('#score');
 
+// Creat logic for start and restart web page. Boots all score from localStorage
+
+function startApp() {
+  setScore(getScore());
+}
+
+function setScore(scoreValue) {
+  localStorage.setItem('score', scoreValue);
+  score.textContent = scoreValue;
+}
+
+function getScore() {
+  return Number(localStorage.getItem('score')) ?? 0;
+}
+
+//addOneVal() - implement +1 to amount of echievement
+
+function addOneVal() {
+  setScore(getScore() + 1);
+}
+
+//visual actions for Click on Lizard or Frog
 circle.addEventListener('click', (event) => {
   event.preventDefault();
 
@@ -10,7 +32,7 @@ circle.addEventListener('click', (event) => {
   const offsetX = event.clientX - rect.left - rect.width / 2;
   const offsetY = event.clientY - rect.top - rect.height / 2;
 
-  const DEG = 50;
+  const DEG = 40;
 
   const tiltX = (offsetY / rect.height) * DEG;
   const tiltY = (offsetX / rect.width) * DEG;
@@ -23,7 +45,7 @@ circle.addEventListener('click', (event) => {
     circle.style.setProperty('--tiltY', `0deg`);
   }, 300);
 
-  //adding echievement's marker "+1" near by img
+  //adding achievement's marker "+1" near by img
 
   const plusOneMarker = document.createElement('div');
 
@@ -34,6 +56,9 @@ circle.addEventListener('click', (event) => {
 
   circle.parentElement.appendChild(plusOneMarker);
 
+  // launch func that increases amount of Achievement
+  addOneVal();
+
   setTimeout(() => {
     plusOneMarker.remove();
   }, 2000);
@@ -42,6 +67,10 @@ circle.addEventListener('click', (event) => {
   circle.style.boxShadow = '0px 0px 20px 10px rgba(238, 145, 31, 0.2)';
 
   setTimeout(() => {
-    circle.style.boxShadow = '0px 0px 20px 0px rgba(129, 68, 172, 0.4)';
+    circle.style.boxShadow = '0px 0px 20px 0px rgba(89, 62, 102, 0.48)';
   }, 100);
 });
+
+//start app and boots all echievements
+
+startApp();
