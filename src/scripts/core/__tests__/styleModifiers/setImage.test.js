12d9e75272ed216b5ed3be95a14094318dea6848
Aleksandr Oliferchuk <oliferchuk.aleksandr@gmail.com>
2024-11-26 18:22:09 +0200

    Testing:refactor: Style Modifiers - setImage
    mocked CIRCLE_ELEMENT and tested with various getScore values

--- src/scripts/core/__tests__/styleModifiers/setImage.test.js
+++ src/scripts/core/__tests__/styleModifiers/setImage.test.js
@@ -1,5 +1,20 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { CIRCLE_ELEMENT } from '../../../utils/constants.js';
 import { setImage } from '../../styleModifiers.js';
 import { getScore } from '../../scoreCalculations.js';
+
+let mockCircleElement;
+
+jest.mock('../../../utils/constants.js', () => {
+  return {
+    get CIRCLE_ELEMENT() {
+      return mockCircleElement;
+    },
+  };
+});
 
 jest.mock('../../scoreCalculations.js', () => {
   return {
@@ -7,6 +22,38 @@ jest.mock('../../scoreCalculations.js', () => {
   };
 });
 
-afterEach(() => {
-  jest.clearAllMocks();
+describe('Testing setImage with different getScore values', () => {
+  beforeEach(() => {
+    mockCircleElement = document.createElement('img');
+    mockCircleElement.setAttribute('src', './assets/frog.png');
+    document.body.appendChild(mockCircleElement);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+    document.body.innerHTML = '';
+  });
+
+  test('getScore value > 50, image will change on lizard', () => {
+    getScore.mockReturnValue(69);
+    setImage();
+
+    expect(CIRCLE_ELEMENT.getAttribute('src')).toBe('./assets/lizzard.png');
+  });
+
+  test('getScore value = 50, image will change on lizard', () => {
+    getScore.mockReturnValue(50);
+    setImage();
+
+    expect(CIRCLE_ELEMENT.getAttribute('src')).toBe('./assets/lizzard.png');
+  });
+
+  test('getScore value < 50, image will change on lizard', () => {
+    getScore.mockReturnValue(34);
+    setImage();
+
+    expect(CIRCLE_ELEMENT.getAttribute('src')).not.toBe('./assets/lizzard.png');
+
+    expect(CIRCLE_ELEMENT.getAttribute('src')).toBe('./assets/frog.png');
+  });
 });
